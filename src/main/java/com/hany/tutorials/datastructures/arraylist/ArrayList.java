ec4df2887cc3079e3e50a6f8c5e50ed2d658737c
hanmanthreddygurrala <46065120+hanmanthreddygurrala@users.noreply.github.com>
2018-12-23 19:36:04 -0800

    arraylist added constructor with size parameter

--- src/main/java/com/hany/tutorials/datastructures/arraylist/ArrayList.java
+++ src/main/java/com/hany/tutorials/datastructures/arraylist/ArrayList.java
@@ -2,7 +2,14 @@ package com.hany.tutorials.datastructures.arraylist;
 
 public class ArrayList {
 
+	private static final int DEFAULTSIZE = 10;	
+	private int[] array = null;
+	
     public ArrayList(){
-
+    	array = new int[DEFAULTSIZE];
+    }
+    
+    public ArrayList(int size) {
+    	array = new int[size];
     }
 }
