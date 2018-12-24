7302fff4be4f8e0020139831884633243b18383a
hanmanthreddygurrala <46065120+hanmanthreddygurrala@users.noreply.github.com>
2018-12-23 19:57:13 -0800

    bugfix commit for parameterized constructor

--- src/main/java/com/hany/tutorials/datastructures/arraylist/ArrayList.java
+++ src/main/java/com/hany/tutorials/datastructures/arraylist/ArrayList.java
@@ -2,14 +2,18 @@ package com.hany.tutorials.datastructures.arraylist;
 
 public class ArrayList {
 
-	private static final int DEFAULTSIZE = 10;	
+	private static final int DEFAULTSIZE = 10;
 	private int[] array = null;
-	
-    public ArrayList(){
-    	array = new int[DEFAULTSIZE];
-    }
-    
-    public ArrayList(int size) {
-    	array = new int[size];
-    }
+
+	public ArrayList() {
+		array = new int[DEFAULTSIZE];
+	}
+
+	public ArrayList(int size) {
+		if (size > 0) {
+			array = new int[size];
+		}else {
+			throw new RuntimeException("Invalid size");
+		}
+	}
 }
