9bd52393b2e9e45c1d3349a9a04ba87bb61b6eee
Divya Garrapally <dgarrapally@expedia.com>
2018-12-23 19:50:11 -0800

    added the method addElement

--- src/main/java/com/hany/tutorials/datastructures/arraylist/ArrayList.java
+++ src/main/java/com/hany/tutorials/datastructures/arraylist/ArrayList.java
@@ -4,6 +4,7 @@ public class ArrayList {
 
 	private static final int DEFAULTSIZE = 10;	
 	private int[] array = null;
+	private int head =0;
 	
     public ArrayList(){
     	array = new int[DEFAULTSIZE];
@@ -11,5 +12,9 @@ public class ArrayList {
     
     public ArrayList(int size) {
     	array = new int[size];
+    }
+
+    public void addElement(int num){
+        array[head++]=num;
     }
 }
